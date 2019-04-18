da0516bc50ed452ef639e5684f2ca97b5a379636
Operel <oriperelman@gmail.com>
2019-04-18 12:17:26 +0300

    add robots CardList

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import './Card.css'
 
 class Card extends Component {
     render () {
         const { name, email, id } = this.props
         return (
-            <div className='Card tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-                <img src={`https://robohash.org/${id}?size=200x200`} alt='robot' />
+            <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+                <img src={`https://robohash.org/${id}?size=220x220`} alt='robot' />
                 <div>
-                    <h2>{name}</h2>
+                    <h3>{name}</h3>
                     <p>{email}</p>
                 </div>
             </div>
---
+++ src/CardList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Card from './Card'
+
+class CardList extends Component {
+    render () {
+        const { robots } = this.props;
+        return (
+            <div>
+                {
+                    robots.map((user, i) => {
+                        return (
+                            <Card
+                            key={i}
+                            id={user.id}
+                            name={user.name}
+                            email={user.email}
+                            />
+                        );
+                    })
+                };
+            </div>
+        );
+    };
+};
+
+export default CardList;
