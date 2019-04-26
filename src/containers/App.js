4ca408b3b56685d668b19b72ef53684a7dca57be
Operel <oriperelman@gmail.com>
2019-04-26 16:21:01 +0300

    add folder structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
-import CardList from './CardList'
-import SearchBox from './SearchBox'
-import Scroll from './Scroll'
-// import { robots } from './robots'
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+import Scroll from '../components/Scroll'
 
 class App extends Component {
     constructor () {
